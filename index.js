4e09769a21f12ce8dfcdbd6f0fb44fe9407c68a4
Shravanthi <57304916+shravanthi94@users.noreply.github.com>
2020-09-09 17:17:08 -0700

    Added Restaurant-Signup/Register Route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require("./app");
 
 const registerCustomer = require("./routers/customer/register");
 const loginCustomer = require("./routers/customer/login");
+const registerRestaurant = require("./routers/restaurant/register");
 
 //Routes for Customers
 
@@ -9,6 +10,11 @@ const loginCustomer = require("./routers/customer/login");
 app.use("/customer/register", registerCustomer);
 //Customer - LOGIN
 app.use("/customer/login", loginCustomer);
+
+//Routes for Restaurant
+
+//Restaurant SIGNUP
+app.use("/restaurant/register", registerRestaurant);
 
 //Connection to a port
 const PORT = process.env.PORT || 3000;
