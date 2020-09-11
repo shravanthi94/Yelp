b248a7465df8f3e88d970e2872b35d92c1ed1500
Shravanthi <57304916+shravanthi94@users.noreply.github.com>
2020-09-11 11:18:07 -0700

    Added Customer-Profile Update Contact Information Route

--- routers/customer/profile.js
+++ routers/customer/profile.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const dbPool = require("../../config/db");
 const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
 // @route  GET yelp/customer/profile/all
 // @desc   Get all customer profile details
@@ -159,7 +160,7 @@ router.post("/about", auth, (req, res) => {
         console.log(error.sqlMessage);
         return res.status(500).send("Server Error");
       }
-      res.send("About me details added.");
+      res.status(200).send("About me details added.");
     });
   } catch (err) {
     console.log(err);
@@ -183,21 +184,62 @@ router.put("/about", auth, (req, res) => {
   } = req.body;
 
   try {
-    const updateAboutQuery = `UPDATE customer_about_data set yelping_since='${yelping_since}', things_i_love='${things_i_love}', 
-    find_me_in='${find_me_in}', my_blog='${my_blog}', when_not_yelping='${when_not_yelping}', 
-    why_read_my_reviews='${why_read_my_reviews}', recent_discovery='${recent_discovery}' WHERE (customer_id=${customer_id})`;
+    const updateAboutQuery = `UPDATE customer_about_data set yelping_since='${yelping_since}', 
+    things_i_love='${things_i_love}', find_me_in='${find_me_in}', my_blog='${my_blog}', 
+    when_not_yelping='${when_not_yelping}', why_read_my_reviews='${why_read_my_reviews}', 
+    recent_discovery='${recent_discovery}' WHERE (customer_id=${customer_id})`;
 
     dbPool.query(updateAboutQuery, (error, result) => {
       if (error) {
         console.log(error.sqlMessage);
         return res.status(500).send("Server Error");
       }
-      res.send("About me details updated.");
+      res.status(200).send("About me details updated.");
     });
   } catch (err) {
     console.log(err);
     res.status(500).send("Server Error.");
   }
 });
+
+// @route  POST yelp/customer/profile/contact
+// @desc   Update current customer contact information
+// @access Private
+router.post(
+  "/contact",
+  [
+    auth,
+    [
+      check("customer_email_id", "Customer email is required.")
+        .notEmpty()
+        .isEmail()
+    ]
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const customer_id = req.user.id;
+    const { customer_email_id, customer_phone } = req.body;
+
+    try {
+      const contactUpdateQuery = `UPDATE customer set customer_email_id='${customer_email_id}', 
+    customer_phone='${customer_phone}' WHERE customer_id = ${customer_id}`;
+
+      dbPool.query(contactUpdateQuery, (error, result) => {
+        if (error) {
+          console.log(error.sqlMessage);
+          return res.status(500).send("Server Error");
+        }
+        res.status(200).send("Contact details updated.");
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
