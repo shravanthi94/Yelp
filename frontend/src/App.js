fd7d8175ed6d068b3860e110e46a4b6855d27063
Shravanthi <57304916+shravanthi94@users.noreply.github.com>
2020-09-23 19:30:24 -0700

    Added authentication part on load

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Landing from './components/LandingPage/Landing';
@@ -9,22 +9,34 @@ import Signup from './components/Customer/Signup';
 import { Provider } from 'react-redux';
 import store from './store';
 import './App.css';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Switch>
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/signup' component={Signup} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  //  Component did mount
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Switch>
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signup' component={Signup} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -1,7 +1,28 @@
-import { SIGNUP_SUCCESS, SIGNUP_FAIL } from './types';
+import { SIGNUP_SUCCESS, SIGNUP_FAIL, USER_LOADED, AUTH_ERROR } from './types';
 import { setAlert } from './alert';
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
 
+//  Load User
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get('/customer/profile');
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
+//  Customer Signup
 export const signup = ({ name, email, password }) => async (dispatch) => {
   const config = {
     headers: {
@@ -13,6 +34,8 @@ export const signup = ({ name, email, password }) => async (dispatch) => {
 
   try {
     const res = await axios.post('/customer/register', body, config);
+
+    dispatch(setAlert('Successfully registered', 'success'));
 
     dispatch({
       type: SIGNUP_SUCCESS,
